5142f650ffc3a3c5e62eaa70009b261e02b88878
gmahima <mahimagangavarapu@gmail.com>
2020-06-18 17:40:13 +0530

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <button className="btn btn-teal"> find friends</button>
+      <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative my-5" role="alert">
+        <strong className="font-bold">Alert!</strong>
+        <span className="block sm:inline">Please update password</span>
+      </div>
+      <div className="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl p-2 items-center">
+        <div className="flex-shrink-0">
+          <img  src="https://source.unsplash.com/random" alt='dsfasd' className="h-12 w-12 " />
+        </div>
+        <div className="ml-6 pt-1 flex-1">
+          <h4 className="text-xl text-gray-900">Mahima Gangavarapu</h4>
+          <p className="text-base text-gray-600">you have a message</p>
+        </div>
+      </div>
+
     </div>
+  
   );
 }
 
